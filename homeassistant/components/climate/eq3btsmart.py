6b40a7a27b8f2e2894f729bbc1e5ef45e1a97dc5
Janne Grunau <j@jannau.net>
2016-12-11 21:20:43 +0100

    eq3btsmart: support setting operation mode and away mode
    Also reporting low battery warning, valve state, operational lock and
    window open as attributes.

--- homeassistant/components/climate/eq3btsmart.py
+++ homeassistant/components/climate/eq3btsmart.py
@@ -8,18 +8,27 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.components.climate import ClimateDevice, PLATFORM_SCHEMA
+from homeassistant.components.climate import ClimateDevice, PLATFORM_SCHEMA, \
+    STATE_AUTO
 from homeassistant.const import (
     CONF_MAC, TEMP_CELSIUS, CONF_DEVICES, ATTR_TEMPERATURE)
 from homeassistant.util.temperature import convert
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['bluepy_devices==0.2.0']
+from bluepy_devices.devices.eq3btsmart import EQ3BTSMART_AUTO, \
+    EQ3BTSMART_MANUAL, EQ3BTSMART_BOOST, EQ3BTSMART_AWAY, EQ3BTSMART_CLOSED, \
+    EQ3BTSMART_OPEN
+
+REQUIREMENTS = ['bluepy_devices==0.3.0']
 
 _LOGGER = logging.getLogger(__name__)
 
-ATTR_MODE = 'mode'
-ATTR_MODE_READABLE = 'mode_readable'
+EQ3BT_ATTR_MODE = 'mode'
+EQ3BT_ATTR_MODE_READABLE = 'mode_readable'
+EQ3BT_ATTR_VALVE_STATE = 'Valve'
+EQ3BT_ATTR_LOCKED = 'Locked'
+EQ3BT_ATTR_WINDOW = 'WindowOpen'
+EQ3BT_ATTR_BATTERY = 'LowBattery'
 
 DEVICE_SCHEMA = vol.Schema({
     vol.Required(CONF_MAC): cv.string,
@@ -29,6 +38,22 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_DEVICES):
         vol.Schema({cv.string: DEVICE_SCHEMA}),
 })
+
+
+STATE_MANUAL = "Manual"
+STATE_BOOST = "Boost"
+STATE_AWAY = "Away"
+STATE_CLOSED = "Off"
+STATE_OPEN = "On"
+
+EQ3BT_STATE_MAP = {
+    EQ3BTSMART_AUTO: STATE_AUTO,
+    EQ3BTSMART_MANUAL: STATE_MANUAL,
+    EQ3BTSMART_BOOST: STATE_BOOST,
+    EQ3BTSMART_AWAY: STATE_AWAY,
+    EQ3BTSMART_CLOSED: STATE_CLOSED,
+    EQ3BTSMART_OPEN: STATE_OPEN,
+}
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -52,6 +77,7 @@ class EQ3BTSmartThermostat(ClimateDevice):
 
         self._name = _name
         self._thermostat = eq3btsmart.EQ3BTSmartThermostat(_mac)
+        self._away = None
 
     @property
     def name(self):
@@ -62,6 +88,25 @@ class EQ3BTSmartThermostat(ClimateDevice):
     def temperature_unit(self):
         """Return the unit of measurement that is used."""
         return TEMP_CELSIUS
+
+    @property
+    def current_operation(self):
+        """Return current operation ie. auto, manual, boost, away."""
+        return EQ3BT_STATE_MAP[self._thermostat.mode]
+
+    def set_operation_mode(self, operation_mode):
+        """Set new target operation mode."""
+        for mode, op_mode in EQ3BT_STATE_MAP.items():
+            if op_mode == operation_mode:
+                self._thermostat.mode = mode
+                break
+
+    @property
+    def operation_list(self):
+        """List of available operation modes."""
+        op_list = [STATE_AUTO, STATE_MANUAL, STATE_BOOST,
+                   STATE_CLOSED, STATE_OPEN]
+        return op_list
 
     @property
     def current_temperature(self):
@@ -84,9 +129,28 @@ class EQ3BTSmartThermostat(ClimateDevice):
     def device_state_attributes(self):
         """Return the device specific state attributes."""
         return {
-            ATTR_MODE: self._thermostat.mode,
-            ATTR_MODE_READABLE: self._thermostat.mode_readable,
+            EQ3BT_ATTR_MODE: self._thermostat.mode,
+            EQ3BT_ATTR_MODE_READABLE: self._thermostat.mode_readable,
+            EQ3BT_ATTR_VALVE_STATE: self._thermostat.valve_state,
+            EQ3BT_ATTR_LOCKED: self._thermostat.locked,
+            EQ3BT_ATTR_WINDOW: self._thermostat.window_open,
+            EQ3BT_ATTR_BATTERY: self._thermostat.low_battery,
         }
+
+    @property
+    def is_away_mode_on(self):
+        """Return if away mode is on."""
+        return self._away
+
+    def turn_away_mode_on(self):
+        """Turn away on."""
+        self._thermostat.mode = EQ3BTSMART_AWAY
+        self._away = True
+
+    def turn_away_mode_off(self):
+        """Turn away off."""
+        self._thermostat.mode = EQ3BTSMART_AUTO
+        self._away = False
 
     @property
     def min_temp(self):
@@ -103,3 +167,4 @@ class EQ3BTSmartThermostat(ClimateDevice):
     def update(self):
         """Update the data from the thermostat."""
         self._thermostat.update()
+        self._away = self._thermostat.mode == EQ3BTSMART_AWAY
